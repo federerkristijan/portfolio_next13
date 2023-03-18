faf809758233fff555a0a456b43b3db7ca6ed5ac
federerkristijan <federer.kristijan@gmail.com>
2023-03-18 09:21:53 +0100

    navbar setup

--- app/shared/Navbar.jsx
+++ app/shared/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duraction: 1 }}
+        className="flex items-center"
+      >
+        
+      </motion.div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
