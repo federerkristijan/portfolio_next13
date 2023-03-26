9c8151f9bc2230dc894b3e5fd9c045d344965b92
federerkristijan <federer.kristijan@gmail.com>
2023-03-26 23:35:11 +0200

    intro.jsx css done

--- app/container/Intro.jsx
+++ app/container/Intro.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className='flex flex-col items-center justify-center lg:grid lg:grid-cols-2 gap-20 p-10 relative w-full h-full overflow-hidden'>
+
+    </div>
   )
 }
 
