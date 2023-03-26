34c3b8f201d1fe8fdd7e836ed62a628e17c990a9
federerkristijan <federer.kristijan@gmail.com>
2023-03-26 23:36:59 +0200

    motion.div added

--- app/container/Intro.jsx
+++ app/container/Intro.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { motion } from "framer-motion";
 
 const Intro = () => {
   return (
     <div className='flex flex-col items-center justify-center lg:grid lg:grid-cols-2 gap-20 p-10 relative w-full h-full overflow-hidden'>
-
+       <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="col-span-1 flex flex-col justify-center"
+      ></motion.div>
     </div>
   )
 }
