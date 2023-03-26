e81738d161f59fb546508af865a21242ce52b817
federerkristijan <federer.kristijan@gmail.com>
2023-03-26 23:39:59 +0200

    css added to h3

--- app/container/Intro.jsx
+++ app/container/Intro.jsx
@@ -10,7 +10,7 @@ const Intro = () => {
         className="col-span-1 flex flex-col justify-center"
       >
         <div className='flex mb-5'>
-          <h3>Kristijan Federer</h3>
+          <h3 className="text-3xl sm:text-7xl font-extralight hover:animate-bounce transition duration-500 delay-200 sm:tracking-wider hover:mytext">Kristijan Federer</h3>
         </div>
       </motion.div>
     </div>
