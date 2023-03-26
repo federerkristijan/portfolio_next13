519db6d739f91af02bce1de8affbe665fec1d93d
federerkristijan <federer.kristijan@gmail.com>
2023-03-26 23:43:41 +0200

    page.js import and implention sorted out

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Intro from "./container/Intro";
 
 export default function Home() {
   return (
     <div className='text-slate-500 myh overflow-y-scroll overflow-x-hidden z-0 snap-mandatory snap-y scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-purple-500'>
-      Hello World
+      <section id="intro" className="myh snap-center">
+        <Intro />
+      </section>
     </div>
   )
 }
